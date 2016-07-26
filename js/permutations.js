12b363f4487a870b4e0545a998e0f2ace931ded4
Muggy Ate <muggy8@gmail.com>
2016-07-25 18:00:26 -0700

    recursion unpacker written

--- js/permutations.js
+++ js/permutations.js
@@ -1,5 +1,21 @@
 function transmute(cssCluster){
 	var returnCluster = [];
-	
 	var permutationRegex = /\<[a-zA-Z]?[\w\/\\\-\_]*\>/;
+	var foundCluster = cssCluster.match(permutationRegex);
+	
+	if (foundCluster !== null){
+		
+		var clusterList = foundCluster[0].substr(1, foundCluster[0].length-2).split("/");
+		//console.log(clusterList);
+		for (var i = 0; i < clusterList.length; i++){
+			//console.log(foundCluster.input.replace(permutationRegex, clusterList[i]));
+			returnCluster = returnCluster.concat(transmute( foundCluster.input.replace(permutationRegex, clusterList[i]) ));
+		}
+		
+	}
+	else{
+		return "." + cssCluster;
+	}
+	
+	return returnCluster;
 }
